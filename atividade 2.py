195dbb1df13e0ae852a89e07fd528b923514db33
Henrique <69260698+HenriqueSilva29@users.noreply.github.com>
2020-11-10 11:27:25 -0300

    :rocket: feat: atividade 2

---
+++ atividade 2.py
@@ -0,0 +1,4 @@
+primeiroNumero = int(input(" Um número qualquer :"))
+segundoNumero  = int(input("Outro número qualquer :"))
+multiplicacao = primeiroNumero*segundoNumero
+print(primeiroNumero,"multiplicado com",segundoNumero,"é igual a ",multiplicacao)
